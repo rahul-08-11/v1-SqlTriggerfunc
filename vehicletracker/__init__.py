f4565ef75b230047942a017b154b99437d97e0a3
rahul-08-11 <grahulkumar2002@gmail.com>
2024-09-19 15:40:36 +0530

    added delete func

--- vehicletracker/__init__.py
+++ vehicletracker/__init__.py
@@ -3,53 +3,73 @@ import logging
 import requests
 
 def add_ifbid_vehicles(details):
-    data = {
-        "Name":"test",
-        "VIN": details.get("Vin"),
-        "Make": details.get("Make"),
-        "Model": details.get("Model"),
-        "Price": details.get("Reserve Price"),
-        # "Seller_ID": details.get("Consigner"),
-        # "vehicle_url": details.get("Vehicle URL"),
-        "Year": details.get("Vehicle Year"),
-        "Mileage": details.get("Odometer Amount"),
-        "Trim": details.get("Trim"),
-        "Declarations": details.get("Vehicle Declarations"),
-        # "vehicle_description": details.get("Vehicle Description"),
-        # "vehicle_condition_score": details.get("Vehicle Condition Score"),
-        # "vehicle_capture_type": details.get("Vehicle Capture Type"),
-        "Carfax_URL": details.get("Carfax_URL"),
-        "Carfax": details.get("Carfax_Odo"),
-        "Vehicle_Image_Url": "//example.com",#details.get("Image_Link"),
-        "Source":"IN_IFBID"
-    }
-    url = "https://bubble-service-major.azurewebsites.net/api/register-vehicle-lead?code=Pc80TXSbJT1UL5dmCcZJmFWndDQLKT8-2oqxkK43PgEAAzFuBPsS-A%3D%3D"
+    try:
+        data = {
+            "Name":"test",
+            "VIN": details.get("Vin"),
+            "Make": details.get("Make"),
+            "Model": details.get("Model"),
+            "Auction_Date": details.get("Auction Date"),
+            "Price": details.get("Reserve Price"),
+            "SellerName": details.get("Consigner"),
+            "Auction_URL": details.get("Auction Item Id"),
+            "Year": details.get("Vehicle Year"),
+            "Mileage": details.get("Odometer Amount"),
+            "Trim": details.get("Trim"),
+            "Declarations": details.get("Vehicle Declarations"),
+            "VehicleDescription": details.get("Vehicle Description"),
+            "VehicleConditionScore": details.get("Vehicle Condition Score"),
+            "VehicleCaptureType": details.get("Vehicle Capture Type"),
+            "Carfax_URL": details.get("Carfax_URL"),
+            "Vehicle_Image_Url": "//example.com",#details.get("Image_Link"),
+            "Source":"IN_IFBID"
+        }
+        url = "https://bubble-service-major.azurewebsites.net/api/register-vehicle-lead?code=Pc80TXSbJT1UL5dmCcZJmFWndDQLKT8-2oqxkK43PgEAAzFuBPsS-A%3D%3D"
 
-    response = requests.post(url,data=data )
-    logging.info(response.json())
-
-    # apis.VehicleApi.add_vehicle_into_crm(access_token,data)
-    pass
-
+        response = requests.post(url,data=data )
+        logging.info(response.json())
+    
+    except Exception as e:
+        logging.error(f"An error occurred during the vehicle registration: {e}")
 
 def update_vehicle_status(details):
-    new_status = details.get("InStatus")
-    vin = details.get("Vin")
+    try:
+        new_status = details.get("InStatus")
+        vin = details.get("Vin")
 
-    url = "https://bubble-service-major.azurewebsites.net/api/vehicle-update?code=NWFfiNpmUBfEQaW-YfzRz3wjusFP1Z_RC683eEP9BWH0AzFuIcWS4Q%3D%3D"
-    data = {
-        "data":
-            [
+        url = "https://bubble-service-major.azurewebsites.net/api/vehicle-update?code=NWFfiNpmUBfEQaW-YfzRz3wjusFP1Z_RC683eEP9BWH0AzFuIcWS4Q%3D%3D"
+        data = {
+            "data":
+                [
+                    {
+                        "Vin":vin,
+                        "Status":new_status
+                    }
+                ]
+
+            }
+        response = requests.post(url,json=data)
+        logging.info(response.json())
+    except Exception as e:
+        logging.error(f"An error occurred during the vehicle status update: {e}")
+
+
+def delete_vehicle(details):
+    try:
+        vin = details.get("Vin")
+        data = {
+            "data" : [
                 {
-                    "Vin":vin,
-                    "Status":new_status
+                "Vin":vin
                 }
             ]
-
         }
-    response = requests.post(url,json=data)
-    logging.info(response.json())
+        url = "https://bubble-service-major.azurewebsites.net/api/vehicle-delete?code=duroxFxv3kfBGDqPyrN9_lzIgTkvsMj2QpIzGVEdRafbAzFuU3ht1Q%3D%3D"
+        response = requests.post(url,json=data)
+        logging.info(response.json())
 
+    except Exception as e:
+        logging.error(f"An error occurred during the vehicle deletion: {e}")
 
 def main(changes):
     logging.info("SQL Changes: %s", json.loads(changes))
@@ -61,6 +81,9 @@ def main(changes):
 
         elif change['Operation'] == 1:
             update_vehicle_status(change['Item'])
+
+        elif change['Operation'] == 2:
+            delete_vehicle(change['Item'])
             
     #operation 1 means update
     #operation 2 means delete
